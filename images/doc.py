3265c17d557e575faf36703d698cb1f30244916c
FARHAN <farhanfarooq786000@gmail.com>
2022-10-03 21:28:52 +0530

    hey

---
+++ images/doc.py
@@ -0,0 +1,16 @@
+price = 10
+rating = 4.5
+name = 'farhan'
+is_published = True
+print(type(price))
+print(type(rating))
+print(type(name))
+print(type(is_published))
+
+
+input('what is your name? ')
+print('Hi ' + name)
+
+name = input('what is your name? ')
+color = input('what is your favorite color? ')
+print(name +' likes ' + color)
