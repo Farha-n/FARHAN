75ae48f85aed69e4c4cb65b1dbc97a0ea733b35f
FARHAN <farhanfarooq786000@gmail.com>
2022-10-03 22:06:33 +0530

    hey

---
+++ practice.py
@@ -0,0 +1,18 @@
+birth_year = input('Birth year: ')
+age = 2022 - int(birth_year)
+print(type(age))
+print(age)
+
+
+weight_lbs = input('Weight_lbs: ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+
+
+height_meter = input('Height_meter: ')
+height_foot = int(height_meter) * 3.3
+height_inch = int(height_foot) * 12
+height_cm = int(height_inch) * 2.5
+print(height_foot)
+print(height_inch)
+print(height_cm)
