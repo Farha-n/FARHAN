6c992d9af53d8dc39ea057dde4b0b108ca1be966
FARHAN <farhanfarooq786000@gmail.com>
2022-10-08 16:05:29 +0530

    kay

---
+++ PYTHON vs/practice1.py
@@ -0,0 +1,35 @@
+a = 1
+b = 2
+c = 3
+
+if a > b:
+    print("hey")
+
+elif a==c:
+    print("hii")
+
+else:
+    print("none")
+
+a = 1
+b = 2
+c = 3
+
+print("Hello")  if(a>b)  else print("ABCD")
+
+a = 1
+b = 2
+c = 3
+
+if a < b and b < c:
+    print("both are true")
+
+marks = int(input("Enter your marks : "))
+if marks > 90 and marks <= 100:
+    print("Your grade is A")
+elif marks > 80 and marks <=90:
+    print("your grade is B")
+elif marks > 70 and marks <= 80:
+    print("your grade is C")
+else:
+    print("Enter a valid number")
