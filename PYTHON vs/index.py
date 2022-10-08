307c28ac7a80e159575751c2567e65e7ad8e4479
FARHAN <farhanfarooq786000@gmail.com>
2022-10-08 12:52:31 +0530

    'hii'

---
+++ PYTHON vs/index.py
@@ -0,0 +1,32 @@
+x = 2
+
+sqrt = x ** 0.5
+
+print(sqrt)
+
+x = int(input('Enter the number '))
+sqrt = x ** 0.5
+
+print(sqrt)
+
+a = 5
+area_triangle = (3 ** 0.5)/4 * a ** 2
+print(area_triangle)
+
+a = 5
+b = 6
+c = 8
+semi_perimeter = (a + b + c)/2
+s = semi_perimeter
+area_1 = (s * (s-a) * (s-b) * (s-c)) ** 0.5
+
+print(semi_perimeter)
+print(area_1)
+
+a = 3
+b = 2
+c = 4
+s = 4.5
+area = (s * (s-a) * (s-b) * (s-c)) ** 0.5
+print(area)
+
