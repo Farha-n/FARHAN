5ed34f23ed3f59b17e68854634aad48c0ad422df
FARHAN <farhanfarooq786000@gmail.com>
2022-10-15 13:54:59 +0530

    h

---
+++ PYTHON vs/RANDOM_.py
@@ -0,0 +1,12 @@
+import random
+
+print(random.randrange(0,55))
+
+
+String="HELLO_WORLD"
+print(String[2])
+print(String[:-1])
+print(String[3:-2])
+print(String[-1:0])
+print(String[:4])
+print(len(String))
