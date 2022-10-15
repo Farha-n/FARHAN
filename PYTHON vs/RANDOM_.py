66f4e8c7f5a107232133b093e07c48e212702c03
FARHAN <farhanfarooq786000@gmail.com>
2022-10-15 15:26:09 +0530

    h

--- PYTHON vs/RANDOM_.py
+++ PYTHON vs/RANDOM_.py
@@ -10,3 +10,21 @@ print(String[3:-2])
 print(String[-1:0])
 print(String[:4])
 print(len(String))
+
+
+# ARRAY : SIMILAR DATA TYPE
+#LIST : DIFFERENT DATA TYPE
+
+#in
+
+quote = "A quick brown fox jumps over the lazy dog"
+if "quick" in quote:
+    print("Yes")
+else:
+    print("No")
+
+
+if "quick" not in quote:
+    print("yay")
+else:
+    print("nay")
