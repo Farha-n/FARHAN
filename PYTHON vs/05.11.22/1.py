58a78a64265399d08e725a78bc2224fa2da581c8
FARHAN <farhanfarooq786000@gmail.com>
2022-11-05 16:35:23 +0530

    f

---
+++ PYTHON vs/05.11.22/1.py
@@ -0,0 +1,22 @@
+def hello_world():
+    print("Hello world")
+def hello_Mars():
+    print("Hello Mars")
+
+hello_Mars()
+hello_world()
+
+def name(x):
+    print(x)
+
+name(4)
+name(10)
+
+def add (num1 , num2):
+    sum=num1 + num2
+    print(sum)
+
+add(5,10)
+add(100, 17)
+add(10, 15)
+add(1000,5055)
