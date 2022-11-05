878fa6aaf8c1dadc3991348e3b0fe9a3a48cadeb
FARHAN <farhanfarooq786000@gmail.com>
2022-11-05 16:41:13 +0530

    d

---
+++ PYTHON vs/05.11.22/2.py
@@ -0,0 +1,18 @@
+def sub(a,b):
+    sub=a-b
+    print(sub)
+
+sub(25, 10)
+sub(10,15)
+sub(5500,2542)
+
+def multiply(c,d):
+    mul=c*d
+    return(mul)
+
+result = multiply(2, 6)
+result1=multiply(50, 1/5)
+print(result)
+print(int(result1))
+
+
