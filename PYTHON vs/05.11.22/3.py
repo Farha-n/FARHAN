ed9f8195f19d07d50a1ff48896f33962f613ac60
FARHAN <farhanfarooq786000@gmail.com>
2022-11-05 16:42:18 +0530

    f

---
+++ PYTHON vs/05.11.22/3.py
@@ -0,0 +1,18 @@
+def name(fn):
+    print("My name is : " +fn)
+
+name("omkaar")
+
+
+def Name(fn , ln):
+    print("My name is : " + fn + ln)
+
+Name("omkaar", "nath shahdeo")
+
+def NAME(fn = "Vishal"):
+    print("My name is : " + fn)
+
+name("Teja")
+name("Prem")
+name()
+name("Chauhan")
